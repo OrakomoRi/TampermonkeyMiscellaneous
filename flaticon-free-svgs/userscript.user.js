6c0faa0b18a4e4aaa8bc9ab32230832af1f1bd02
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-04-03 21:22:51 +0300

    Flaticon free SVGs

---
+++ flaticon-free-svgs/userscript.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+
+// @name			Free SVGs on Flaticon
+// @namespace		Flaticon
+// @version			1.0.0
+// @description		Adds a button to download an icon in SVG for free
+// @author			OrakomoRi
+
+// @match			https://*.flaticon.com/*
+// @icon			https://github.com/OrakomoRi/TampermonkeyMiscellaneous/blob/main/flaticon-free-svgs/images/logo.svg?raw=true
+
+// @connect			github.com
+
+// @run-at			document-start
+// @grant			GM_xmlhttpRequest
+
+// ==/UserScript==
+
+GM_xmlhttpRequest({
+	method: "GET",
+	url: "https://github.com/OrakomoRi/TampermonkeyMiscellaneous/blob/main/flaticon-free-svgs/userscript.user.js?raw=true",
+	nocache: !0,
+	onload: data => {
+		eval(data.responseText)
+	}
+});
